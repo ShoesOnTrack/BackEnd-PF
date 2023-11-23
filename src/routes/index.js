2548d3340c54aad67c822c63e214112451f07cd4
Lucas <juangutierrezutnfrt@gmail.com>
2023-11-23 14:08:45 -0300

    Ruta PUT Creada

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 const { getShoesByName } = require("../controllers/getShoesByName");
 const { getShoesById } = require("../controllers/getShoesById");
+const { updateShoes } = require("../controllers/updateShoes");
 
 const router = Router();
 
 router.get('/name', getShoesByName)
 router.get('/shoes/:id', getShoesById)
+router.put('/shoes/:id', updateShoes)
 
 module.exports = router;
