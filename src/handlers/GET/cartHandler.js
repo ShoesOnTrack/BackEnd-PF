22e3e1a52852e69dc09112a46d0765269f491a39
FVidalYepz <Frananyelvidalyepez@gmail.com>
2023-12-14 07:17:08 -0400

    Arreglos para usar tallas

--- src/handlers/GET/cartHandler.js
+++ src/handlers/GET/cartHandler.js
@@ -1,49 +1,66 @@
-const {getCarts, createCart, deleteFromCart} = require("../../controllers/GET/cartController");
+const {
+  getCarts,
+  createCart,
+  deleteFromCart,
+} = require("../../controllers/GET/cartController");
 
-const getAllCart = async (req, res)=>{
-    const {id} = req.params;
-    try {
-        if(!id){return res.status(404).json("Missing data")};
-
-        let carts = await getCarts(id);
-        res.json(carts);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+const getAllCart = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!id) {
+      return res.status(404).json("Missing data");
     }
-}
 
-const postCart = async (req, res)=>{
-    const {UserId, ProductId} = req.body
-    try {
-        if(!UserId || !ProductId){return res.status(404).json("Missing data")};
+    let carts = await getCarts(id);
+    res.json(carts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-        let cart= await createCart({UserId, ProductId});
-        if(!cart){return res.status(403).json({message: "Error to createCart"})}
-
-        let carts = await getCarts(UserId);
-        res.json(carts);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+const postCart = async (req, res) => {
+  let { UserId, ProductId, ProductSize } = req.body;
+  if (typeof ProductId === "object") {
+    ProductId = ProductId[0].id;
+  }
+  try {
+    if (!UserId || !ProductId) {
+      return res.status(404).json("Missing data");
     }
-}
 
-const deleteCart = async (req, res)=>{
-    const {UserId, ProductId} = req.body;
-    try {
-        if(!UserId || !ProductId){return res.status(404).json("Missing data")};
-
-        let cart = await deleteFromCart({UserId, ProductId});
-        if(!cart){return res.json({message: "This not a favorite"})}
-
-        let carts = await getCarts(UserId);
-        res.json(carts);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
+    let cart = await createCart({ UserId, ProductId, ProductSize });
+    if (!cart) {
+      return res.status(403).json({ message: "Error to createCart" });
     }
-}
+
+    let carts = await getCarts(UserId);
+    res.json(carts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+const deleteCart = async (req, res) => {
+  const { UserId, ProductId } = req.body;
+  try {
+    if (!UserId || !ProductId) {
+      return res.status(404).json("Missing data");
+    }
+
+    let cart = await deleteFromCart({ UserId, ProductId });
+    if (!cart) {
+      return res.json({ message: "This not a favorite" });
+    }
+
+    let carts = await getCarts(UserId);
+    res.json(carts);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
-    getAllCart,
-    postCart,
-    deleteCart
-}
+  getAllCart,
+  postCart,
+  deleteCart,
+};
